5e6996fdb9e9cd025c98e58533372c0fac375262
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-06 19:50:42 -0300

    (feature) Mejoré gitignore, deploy-to-s3.workflow ahora crea s3 bucket, arreglé typo en workflow.yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,16 +10,17 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-variable "S3_UUID" {
-  type = string
+  alias      = "aws_provider"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  token      = var.aws_session_token
+  region     = var.aws_region
+  profile    = var.aws_profile
 }
 
 resource "aws_s3_bucket" "s3" {
-  bucket = var.S3_UUID 
+  bucket   = var.aws_s3_bucket_name
+  provider = aws.aws_provider
 
   tags = {
     Name = "Frontend React App"
