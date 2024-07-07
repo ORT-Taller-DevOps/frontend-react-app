a5eacb8f9fb803ba54bcfe21b79bd25d23658ac2
Thiago Schreck <thiago.schreck1@gmail.com>
2024-07-06 21:53:27 -0300

    (feature) Continue if bucket already exists, empty bucket before uploading new files

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 cd dist/apps/catalog
-echo "Deploying site to $1"
+
+echo "Removing existing files in $1"
+aws s3 rm s3://$1 --recursive
+
+echo "Uploading files to $1"
 aws s3 sync ./ s3://$1 --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
